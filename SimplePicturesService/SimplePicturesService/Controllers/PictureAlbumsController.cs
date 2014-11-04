b6e1c12d5339af54891ef286c2b9767e5ef26dd7
Arsalan Ahmed <itsarsalan@yahoo.com>
2014-11-03 23:00:25 -0500

    Addec cors support

--- SimplePicturesService/SimplePicturesService/Controllers/PictureAlbumsController.cs
+++ SimplePicturesService/SimplePicturesService/Controllers/PictureAlbumsController.cs
@@ -5,10 +5,12 @@ using System.Net;
 using System.Net.Http;
 using System.Web;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 
 namespace SimplePicturesService.Controllers
 {
+    [EnableCors("*", "*", "*")]
     public class PictureAlbumsController : ApiController
     {
         private static IList<PictureAlbum> pictureAlbums;
